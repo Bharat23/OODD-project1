3727e3e2aede0265fa863e15305c9ba68adbe68a
Bharat Sinha <bharat.sinha.2307@gmail.com>
2019-09-25 10:37:52 -0400

    add schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -1,23 +1,36 @@
-ActiveRecord::Schema.define do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
 
-  create_table "libraries", force: :cascade do |t|
-    t.string "name", uniqueness: true
-    t.string "location"
-    t.integer "borrow_duration"
-    t.float "total_overdue"
-  end
+ActiveRecord::Schema.define(version: 2019_09_25_042020) do
 
-  create_table "universities", force: :cascade do |t|
-    t.string "name", uniqueness: true
-  end
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
-  create_table "university_library_mapping", force: :cascade do |t|
-    t.references :libraries, foreign_key: true
-    t.references :universities, foreign_key: true
+  create_table "book_issue_transactions", force: :cascade do |t|
+    t.bigint "students_id"
+    t.bigint "books_id"
+    t.bigint "libraries_id"
+    t.string "status"
+    t.date "due_date"
+    t.date "last_updated"
+    t.string "reason"
+    t.float "fine_amount"
+    t.index ["books_id"], name: "index_book_issue_transactions_on_books_id"
+    t.index ["libraries_id"], name: "index_book_issue_transactions_on_libraries_id"
+    t.index ["students_id"], name: "index_book_issue_transactions_on_students_id"
   end
 
   create_table "books", force: :cascade do |t|
-    t.string "isbn", uniqueness: true
+    t.string "isbn"
     t.string "title"
     t.string "author"
     t.string "language"
@@ -29,51 +42,68 @@ ActiveRecord::Schema.define do
     t.boolean "special_collection"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "role"
-    t.string "username", uniqueness: true
-    t.string "password"
+  create_table "librarians", force: :cascade do |t|
+    t.string "name"
+    t.bigint "libraries_id"
+    t.index ["libraries_id"], name: "index_librarians_on_libraries_id"
+  end
+
+  create_table "libraries", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.integer "borrow_duration"
+    t.float "total_overdue"
+  end
+
+  create_table "library_book_mapping", force: :cascade do |t|
+    t.bigint "libraries_id"
+    t.bigint "books_id"
+    t.integer "book_count"
+    t.index ["books_id"], name: "index_library_book_mapping_on_books_id"
+    t.index ["libraries_id"], name: "index_library_book_mapping_on_libraries_id"
   end
 
   create_table "students", force: :cascade do |t|
-    t.references :users, foreign_key: true
+    t.bigint "users_id"
     t.string "name"
     t.string "university"
     t.string "educational_level"
     t.integer "borrowing_limit"
-  end
-
-  create_table "librarians", force: :cascade do |t|
-    t.string "name"
-    t.references :libraries, foreign_key: true
-  end
-
-  create_table "bookmarks", force: :cascade do |t|
-    t.references :students, foreign_key: true
-    t.references :books, foreign_key: true
-  end
-
-  create_table "book_issue_transactions", force: :cascade do |t|
-    t.references :students, foreign_key: true
-    t.references :books, foreign_key: true
-    t.references :libraries, foreign_key: true
-    t.string "status", inclusion: {in: ["On Hold","Approved","Denied","Returned"]}
-    t.date "due_date"
-    t.date "last_updated"
-    t.string "reason"
-    t.float "fine_amount"
+    t.index ["users_id"], name: "index_students_on_users_id"
   end
 
   create_table "transaction_log", force: :cascade do |t|
-    t.references :books, foreign_key: true
-    t.references :users, foreign_key: true
+    t.bigint "books_id"
+    t.bigint "users_id"
     t.string "action"
     t.datetime "timestamp_of_action"
+    t.index ["books_id"], name: "index_transaction_log_on_books_id"
+    t.index ["users_id"], name: "index_transaction_log_on_users_id"
   end
 
-  create_table "library_book_mapping", force: :cascade do |t|
-    t.references :libraries, foreign_key: true
-    t.references :books, foreign_key: true
-    t.integer "book_count"
+  create_table "universities", force: :cascade do |t|
+    t.string "name"
   end
+
+  create_table "university_library_mapping", force: :cascade do |t|
+    t.bigint "libraries_id"
+    t.bigint "universities_id"
+    t.index ["libraries_id"], name: "index_university_library_mapping_on_libraries_id"
+    t.index ["universities_id"], name: "index_university_library_mapping_on_universities_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "role"
+    t.string "username"
+    t.string "password"
+  end
+
+  add_foreign_key "book_issue_transactions", "libraries", column: "libraries_id"
+  add_foreign_key "book_issue_transactions", "students", column: "students_id"
+  add_foreign_key "librarians", "libraries", column: "libraries_id"
+  add_foreign_key "library_book_mapping", "libraries", column: "libraries_id"
+  add_foreign_key "students", "users", column: "users_id"
+  add_foreign_key "transaction_log", "users", column: "users_id"
+  add_foreign_key "university_library_mapping", "libraries", column: "libraries_id"
+  add_foreign_key "university_library_mapping", "universities", column: "universities_id"
 end
