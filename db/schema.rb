289e42c16ff43eb896b4e577350bdf901340f278
Bharat Sinha <bharat.sinha.2307@gmail.com>
2019-09-25 20:05:05 -0400

    change dev to sqllite

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_25_042020) do
+ActiveRecord::Schema.define(version: 2019_09_25_144136) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "book_issue_transactions", force: :cascade do |t|
-    t.bigint "students_id"
-    t.bigint "books_id"
-    t.bigint "libraries_id"
-    t.string "status"
-    t.date "due_date"
-    t.date "last_updated"
-    t.string "reason"
-    t.float "fine_amount"
-    t.index ["books_id"], name: "index_book_issue_transactions_on_books_id"
-    t.index ["libraries_id"], name: "index_book_issue_transactions_on_libraries_id"
-    t.index ["students_id"], name: "index_book_issue_transactions_on_students_id"
+  create_table "bookmarks", force: :cascade do |t|
+    t.integer "students_id"
+    t.integer "books_id"
+    t.index ["books_id"], name: "index_bookmarks_on_books_id"
+    t.index ["students_id"], name: "index_bookmarks_on_students_id"
   end
 
   create_table "books", force: :cascade do |t|
@@ -42,68 +32,12 @@ ActiveRecord::Schema.define(version: 2019_09_25_042020) do
     t.boolean "special_collection"
   end
 
-  create_table "librarians", force: :cascade do |t|
-    t.string "name"
-    t.bigint "libraries_id"
-    t.index ["libraries_id"], name: "index_librarians_on_libraries_id"
-  end
-
-  create_table "libraries", force: :cascade do |t|
-    t.string "name"
-    t.string "location"
-    t.integer "borrow_duration"
-    t.float "fine_per_day"
-  end
-
-  create_table "library_book_mappings", force: :cascade do |t|
-    t.bigint "libraries_id"
-    t.bigint "books_id"
-    t.integer "book_count"
-    t.index ["books_id"], name: "index_library_book_mapping_on_books_id"
-    t.index ["libraries_id"], name: "index_library_book_mapping_on_libraries_id"
-  end
-
-  create_table "students", force: :cascade do |t|
-    t.bigint "users_id"
-    t.string "name"
-    t.string "university"
-    t.string "educational_level"
-    t.integer "borrowing_limit"
-    t.index ["users_id"], name: "index_students_on_users_id"
-  end
-
-  create_table "transaction_logs", force: :cascade do |t|
-    t.bigint "books_id"
-    t.bigint "users_id"
-    t.string "action"
-    t.datetime "timestamp_of_action"
-    t.index ["books_id"], name: "index_transaction_log_on_books_id"
-    t.index ["users_id"], name: "index_transaction_log_on_users_id"
-  end
-
   create_table "universities", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "university_library_mappings", force: :cascade do |t|
-    t.bigint "libraries_id"
-    t.bigint "universities_id"
-    t.index ["libraries_id"], name: "index_university_library_mapping_on_libraries_id"
-    t.index ["universities_id"], name: "index_university_library_mapping_on_universities_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "role"
-    t.string "username"
-    t.string "password"
-  end
-
-  add_foreign_key "book_issue_transactions", "libraries", column: "libraries_id"
-  add_foreign_key "book_issue_transactions", "students", column: "students_id"
-  add_foreign_key "librarians", "libraries", column: "libraries_id"
-  add_foreign_key "library_book_mapping", "libraries", column: "libraries_id"
-  add_foreign_key "students", "users", column: "users_id"
-  add_foreign_key "transaction_log", "users", column: "users_id"
-  add_foreign_key "university_library_mapping", "libraries", column: "libraries_id"
-  add_foreign_key "university_library_mapping", "universities", column: "universities_id"
+  add_foreign_key "bookmarks", "books", column: "books_id"
+  add_foreign_key "bookmarks", "students", column: "students_id"
 end
