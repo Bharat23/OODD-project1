60158078e0069198a8ce8354beae64a13a4489ad
Tanvi <tmpandi2@ncsu.edu>
2019-09-26 20:49:23 -0400

    Added login facility

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+      sessions: 'users/sessions'
+  }, path_names: {
+      sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', registration: 'register'
+  }
   resources :libraries
   resources :universities
   match '/books/actions/search/' => 'books#search', :via => :get
   resources :books
-    
+  root to: "books#index"
   # end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
