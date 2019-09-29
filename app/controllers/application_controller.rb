e4797bdf4106a03fa8ffffef0f621c75089275d9
Vishva Shah <vishva1001@gmail.com>
2019-09-29 17:57:01 -0400

    Fixed bug that did not allow Edit registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-
+    before_filter :authenticate_user!
     def setup_users
         user = User.new(
             email: 'bsinha2@ncsu.edu', 
@@ -33,15 +33,7 @@ class ApplicationController < ActionController::Base
     end
 
     protected
-    def authenticate_user!
-        if user_signed_in?
-            super
-        else
-            redirect_to new_user_session_path, :notice => 'Please login first'
-            ## if you want render 404 page
-            ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
-        end
-    end
+
 
     def is_admin!
         if user_signed_in?
