6e7ff672e891ed0cc3de4a17948c99cdcee67d49
Bharat Sinha <bharat.sinha.2307@gmail.com>
2019-09-29 17:48:11 -0400

    fix migration issue

--- db/migrate/20190926024407_create_book_issue_transactions.rb
+++ db/migrate/20190926024407_create_book_issue_transactions.rb
@@ -1,7 +1,7 @@
 class CreateBookIssueTransactions < ActiveRecord::Migration[6.0]
   def change
     create_table :book_issue_transactions do |t|
-      t.references :students, foreign_key: true
+      t.references :users, foreign_key: true
       t.references :books, foreign_key: true
       t.references :libraries, foreign_key: true
       t.string "status"
--- db/migrate/20190926024842_create_transaction_logs.rb
+++ db/migrate/20190926024842_create_transaction_logs.rb
@@ -2,7 +2,7 @@ class CreateTransactionLogs < ActiveRecord::Migration[6.0]
   def change
     create_table :transaction_logs do |t|
       t.references :books, foreign_key: true
-      t.references :students, foreign_key: true
+      t.references :users, foreign_key: true
       t.string "action"
       t.datetime "timestamp_of_action"
       # t.index ["books_id"], name: "index_transaction_log_on_books_id"
