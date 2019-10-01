3c92efce1b17b44a1dbe4a4a26b05583636ed62c
Bharat Sinha <bharat.sinha.2307@gmail.com>
2019-10-01 08:55:56 -0400

    add test cases for library

--- test/models/librarian_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class LibrarianTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/library_test.rb
+++ test/models/library_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class LibraryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "library should have name" do
+    library = Library.new
+    assert_not library.save
+  end
+
+  test "library fine shouldn't be anything but numeric" do
+    library = Library.new
+    library.name = "New Name"
+    library.fine_per_day = "asdasddad"
+    assert_not library.save
+  end
+
+  test "library book issue duration shouldn't be anything but numeric" do
+    library = Library.new
+    library.name = "New Name"
+    library.borrow_duration = "asdasddad"
+    assert_not library.save
+  end
+
+  test "library should create value without university id" do
+    library = Library.new
+    library.name = "New Name"
+    library.location = "New Location"
+    library.fine_per_day = 1.5
+    library.borrow_duration = 12
+    assert library.save
+  end
 end
