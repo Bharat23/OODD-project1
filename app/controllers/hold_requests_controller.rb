e8e77a98d93d7cd46b2431e5a2b99aef946cdcb9
Vishva Shah <vishva1001@gmail.com>
2019-10-09 03:13:03 -0400

    Hold requests fully working

---
+++ app/controllers/hold_requests_controller.rb
@@ -0,0 +1,21 @@
+class HoldRequestsController < ApplicationController
+
+  def checkout_hold_list
+    @user_id = params[:user_id]
+    if @user_id == 'admin'
+      @list_of_holds = BookIssueTransaction.where('status = ?', 2)
+    else
+      @list_of_holds = BookIssueTransaction.where('status = ? and users_id = ?', 2, @user_id)
+    end
+  end
+
+  def delete_hold_request
+    @stud_id = params[:stud_id]
+    @b_id = params[:b_id]
+    puts '+++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+    BookIssueTransaction.where('status = ? and books_id = ? and users_id = ?', 2, @b_id, @stud_id).update(status: 0)
+    respond_to do |format|
+      format.html { redirect_to request.referrer, notice: 'deleted' }
+    end
+  end
+end
---
+++ test/controllers/hold_requests_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class HoldRequestsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
